e9e985edfa6f89efd8983ff0ecada47a7d712d5a
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-07 16:44:07 +1000

    made some variables for some data

--- client/script.js
+++ client/script.js
@@ -1,15 +1,23 @@
 let body = document.getElementById("background-colour");
 let button = document.getElementById("get-new-avatar");
-
-button.addEventListener('click', changeAvatar);
-
+let cardDetails = document.getElementById("card-details");
 let avatar = document.getElementById("avatar");
 let avatarUrl = `https://api.adorable.io/avatars/285/`;
+
+button.addEventListener('click', changeUserDetails);
+
+function changeUserDetails() {
+  changeAvatar();
+  addCardDetails();
+}
 
 function changeAvatar() {
   avatar.src = `${avatarUrl}${Math.random()}.png`
 }
 
+function addCardDetails() {
+  cardDetails.innerHTML = `<p>Card Type: ${casual.card_type}</p><p>`
+}
 
 
 // let url = "http://api.adorable.io/avatars/285/" //identifier.png needs to be added to end
@@ -20,3 +28,10 @@ function changeAvatar() {
 //   fetch(`${url}${name}.png`).then((response) => response.JSON())
 //   .then(())
 // }
+
+// name: casual.name,
+// address: casual.address,
+// colour: casual.rgb_hex,
+// cardType: casual.card_type,
+// cardNumber: casual.card_number,
+// cardExpiry: casual.card_exp,
