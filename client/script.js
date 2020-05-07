4a7b2ca14eccd1471ebe12b1aec4892837fa8b22
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-07 16:53:30 +1000

    blahbalhblah

--- client/script.js
+++ client/script.js
@@ -18,8 +18,15 @@ function changeAvatar() {
 function addCardDetails() {
   cardDetails.innerHTML = `<p>Card Type: ${casual.card_type}</p><p>`
 }
+// louise do I need to push it to develop?
 
+// i think when they have merged, we need to pull from develop?
+// i ? 
+// its already push to our branch
 
+// i thikn kno wyou can push to our branch that is off of develop
+// push to feature/client-side
+// ok - i think we maybe wait until they have merged onto develop then we pull
 // let url = "http://api.adorable.io/avatars/285/" //identifier.png needs to be added to end
 
 // let name = ; // need to find the name to use as a variable in url
